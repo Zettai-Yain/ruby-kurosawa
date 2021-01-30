5eed8d67b0c1d3ed0b74b66e19aebe04b168b277
Nanabell <marlon.delfin@gmail.com>
2021-01-30 23:53:18 +0100

    Add Initial Project Files

---
+++ src/main/kotlin/com/nanabell/nico/ruby/Application.kt
@@ -0,0 +1,33 @@
+package com.nanabell.nico.ruby
+
+import io.micronaut.runtime.Micronaut.build
+import java.lang.management.ManagementFactory
+
+object Application  {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val builder =build()
+            .args(*args)
+            .packages("com.nanabell.nico.db")
+            .defaultEnvironments("prod")
+
+        if (isDevEnv())
+            builder.environments("prod")
+
+        builder.start()
+    }
+
+    private fun isDevEnv(): Boolean {
+        for (inputArgument in ManagementFactory.getRuntimeMXBean().inputArguments) {
+            if (inputArgument.startsWith("-javaagent")
+                || inputArgument.startsWith("-agentpath")
+                || inputArgument.startsWith("agentlib"))
+                return true
+        }
+
+        return false
+    }
+}
+
+
---
+++ src/test/kotlin/com/nanabell/nico/ruby/RubyTest.kt
@@ -0,0 +1,13 @@
+package com.nanabell.nico.ruby
+
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.kotest.annotation.MicronautTest
+import io.kotest.core.spec.style.StringSpec
+
+@MicronautTest
+class RubyTest(private val application: EmbeddedApplication<*>) : StringSpec({
+
+    "test the server is running" {
+        assert(application.isRunning)
+    }
+})
