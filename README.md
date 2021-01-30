a242ab8a4a30f4fb2c74121e0de2af3b52c1418d
Nanabell <marlon.delfin@gmail.com>
2021-01-30 23:53:00 +0100

    Add Project Control Files

---
+++ README.md
@@ -0,0 +1,36 @@
+## Feature jetty-server documentation
+
+- [Micronaut Jetty Server documentation](https://micronaut-projects.github.io/micronaut-servlet/1.0.x/guide/index.html#jetty)
+
+## Feature hibernate-jpa documentation
+
+- [Micronaut Hibernate JPA documentation](https://micronaut-projects.github.io/micronaut-sql/latest/guide/index.html#hibernate)
+
+## Feature security documentation
+
+- [Micronaut Micronaut Security documentation](https://micronaut-projects.github.io/micronaut-security/latest/guide/index.html)
+
+## Feature jdbc-hikari documentation
+
+- [Micronaut Hikari JDBC Connection Pool documentation](https://micronaut-projects.github.io/micronaut-sql/latest/guide/index.html#jdbc)
+
+## Feature flyway documentation
+
+- [Micronaut Flyway Database Migration documentation](https://micronaut-projects.github.io/micronaut-flyway/latest/guide/index.html)
+
+- [https://flywaydb.org/](https://flywaydb.org/)
+
+## Feature management documentation
+
+- [Micronaut Micronaut Management documentation](https://docs.micronaut.io/latest/guide/index.html#management)
+
+## Feature http-client documentation
+
+- [Micronaut Micronaut HTTP Client documentation](https://docs.micronaut.io/latest/guide/index.html#httpClient)
+
+## Feature cache-caffeine documentation
+
+- [Micronaut Caffeine Cache documentation](https://micronaut-projects.github.io/micronaut-cache/latest/guide/index.html)
+
+- [https://github.com/ben-manes/caffeine](https://github.com/ben-manes/caffeine)
+
---
+++ build.gradle.kts
@@ -0,0 +1,89 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.4.21"
+    id("org.jetbrains.kotlin.kapt") version "1.4.21"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.4.21"
+    id("org.jetbrains.kotlin.plugin.jpa") version "1.4.21"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+    id("io.micronaut.application") version "1.3.2"
+}
+
+version = "1.0.0"
+group = "com.nanabell.nico.db"
+
+val kotlinVersion = project.properties["kotlinVersion"]
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+micronaut {
+    runtime("netty")
+    testRuntime("kotest")
+
+    processing {
+        incremental(true)
+        annotations("com.nanabell.nico.ruby.*")
+    }
+}
+
+noArg {
+    annotation("com.nanabell.nico.ruby.annotation.NoArgConstructor")
+}
+
+dependencies {
+    kapt("io.micronaut.data:micronaut-data-processor")
+    kapt("io.micronaut.security:micronaut-security-annotations")
+
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
+
+    implementation("io.micronaut:micronaut-runtime")
+    implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
+    implementation("io.micronaut:micronaut-management")
+    implementation("io.micronaut:micronaut-validation")
+
+    implementation("io.micronaut:micronaut-http-client")
+    implementation("javax.annotation:javax.annotation-api")
+    implementation("io.micronaut.security:micronaut-security")
+    implementation("io.micronaut.cache:micronaut-cache-caffeine")
+    implementation("io.micronaut.kotlin:micronaut-kotlin-extension-functions")
+
+    implementation("io.micronaut.micrometer:micronaut-micrometer-core")
+    implementation("io.micronaut.micrometer:micronaut-micrometer-registry-influx")
+
+    implementation("io.micronaut.flyway:micronaut-flyway")
+    implementation("io.micronaut.data:micronaut-data-jdbc")
+    implementation("io.micronaut.sql:micronaut-jdbc-hikari")
+    implementation("io.micronaut.sql:micronaut-hibernate-jpa")
+    implementation("io.micronaut.data:micronaut-data-hibernate-jpa")
+    implementation("com.influxdb:influxdb-client-java:1.14.0")
+
+    implementation("org.mongodb:mongodb-driver-sync:4.1.1")
+
+    runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("ch.qos.logback:logback-classic")
+    runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
+}
+
+
+application {
+    mainClass.set("com.nanabell.nico.ruby.Application")
+}
+
+java {
+    sourceCompatibility = JavaVersion.toVersion("14")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions {
+            jvmTarget = "14"
+        }
+    }
+    compileTestKotlin {
+        kotlinOptions {
+            jvmTarget = "14"
+        }
+    }
+
+}
