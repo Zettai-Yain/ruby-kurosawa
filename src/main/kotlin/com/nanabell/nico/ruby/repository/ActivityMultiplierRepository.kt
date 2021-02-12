24bce6897e68307ab02834ed72fc293cebfbb5c4
Nanabell <marlon.delfin@gmail.com>
2021-02-12 14:10:13 +0100

    Re-add ActivityMultiplierRepository#findByIdAndType Query annotation

--- src/main/kotlin/com/nanabell/nico/ruby/repository/ActivityMultiplierRepository.kt
+++ src/main/kotlin/com/nanabell/nico/ruby/repository/ActivityMultiplierRepository.kt
@@ -5,13 +5,13 @@ import com.nanabell.nico.ruby.entitiy.ActivityMultiplierEntity
 import io.micronaut.data.annotation.Query
 import io.micronaut.data.annotation.Repository
 import io.micronaut.data.jpa.repository.JpaRepository
-import java.util.*
 
 @Repository
 interface ActivityMultiplierRepository : JpaRepository<ActivityMultiplierEntity, ActivityMultiplierEntity.ActivityMultiplierId> {
 
     fun findById(id: Long): List<ActivityMultiplierEntity>
 
+    @Query("SELECT e FROM ActivityMultiplierEntity e WHERE e.id = :id AND e.type = :type")
     fun findByIdAndType(id: Long, type: ActivityMultiplierType): ActivityMultiplierEntity?
 
 }
