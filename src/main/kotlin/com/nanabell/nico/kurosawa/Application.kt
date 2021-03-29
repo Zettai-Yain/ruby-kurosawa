3fb4ee4db37b4962552ce7a0233bff2de535a32d
Nanabell <marlon.delfin@gmail.com>
2021-03-29 08:10:42 +0200

    Add Startup Logging informing about Influx status

--- src/main/kotlin/com/nanabell/nico/kurosawa/Application.kt
+++ src/main/kotlin/com/nanabell/nico/kurosawa/Application.kt
@@ -1,41 +1,70 @@
 package com.nanabell.nico.kurosawa
 
+import io.micronaut.context.annotation.Context
 import io.micronaut.runtime.Micronaut.build
+import io.micronaut.runtime.event.ApplicationStartupEvent
+import io.micronaut.runtime.event.annotation.EventListener
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Contact
 import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.info.License
+import org.slf4j.LoggerFactory
 import java.lang.management.ManagementFactory
 
+@Context
 @OpenAPIDefinition(
     info = Info(
-        title = "Nico Yazawa Ruby",
-        version = "1.1.0",
-        description = "Ruby backend for Nico Yazawa"
+        title = "Ruby Kurosawa",
+        version = "1.0.0",
+        description = "User Activity Store/Configuration Service",
+        contact = Contact(name = "Nanabell"),
+        license = License(name = "MIT")
     )
 )
-object Application {
+class Application {
 
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val builder = build()
-            .args(*args)
-            .packages("com.nanabell.nico.db")
-            .defaultEnvironments("prod")
+    private val logger = LoggerFactory.getLogger(Application::class.java)
 
-        if (com.nanabell.nico.kurosawa.Application.isDevEnv())
-            builder.environments("dev")
+    @EventListener
+    fun init(event: ApplicationStartupEvent) {
+        val environment = event.source.environment
 
-        builder.start()
+        environment.getProperty("micronaut.metrics.enabled", Boolean::class.java, false)!!.let {
+            when (it) {
+                true -> logger.info("Metrics Reporting is enabled, Please ensure you have configured a valid Influx token!")
+                false -> logger.warn("Metrics Reporting is disabled! No Metrics will be collected!")
+            }
+        }
     }
 
-    private fun isDevEnv(): Boolean {
-        for (inputArgument in ManagementFactory.getRuntimeMXBean().inputArguments) {
-            if (inputArgument.startsWith("-javaagent")
-                || inputArgument.startsWith("-agentpath")
-                || inputArgument.startsWith("-agentlib")
-            )
-                return true
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val builder = build()
+                .args(*args)
+                .packages("com.nanabell.nico.db")
+                .defaultEnvironments("prod")
+
+            if (isDevEnv())
+                builder.environments("dev")
+
+            builder.start()
         }
 
-        return false
+        private fun isDevEnv(): Boolean {
+            for (inputArgument in ManagementFactory.getRuntimeMXBean().inputArguments) {
+                if (inputArgument.startsWith("-javaagent")
+                    || inputArgument.startsWith("-agentpath")
+                    || inputArgument.startsWith("-agentlib")
+                )
+                    return true
+            }
+
+            return false
+        }
+
     }
+
+
 }
